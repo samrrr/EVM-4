58be3d060e60282e9b7cb8f0f0957d18520df554
samrrr <samrrr@mail.ru>
2017-03-03 17:56:36 +0300

    L2

--- L2/code.cpp
+++ L2/code.cpp
@@ -12,171 +12,24 @@ using namespace std;
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 
-
-class TYPE_DATA
-{
-public:
-	string name, print_st;
-	int bytes;
-	int p_bytes;
-	byte *buf;
-
-	TYPE_DATA()
-	{
-		buf = nullptr;
-	}
-
-	~TYPE_DATA()
-	{
-		delete[]buf;
-		buf = nullptr;
-	}
-
-	void print_bytes()
-	{
-		string res;
-
-		for (int i = 0; i < bytes; i++)
-			for (int r = 0; r < 8; r++)
-			{
-				res = (char)(buf[i] & (1 << r)) + res;
-			}
-
-		for (int i = 0; i < res.length(); i++)
-		{
-			SetConsoleTextAttribute(hConsole, (WORD)(5));
-			if (i <= p_bytes)
-				SetConsoleTextAttribute(hConsole, (WORD)(4));
-			if (i == 0)
-				SetConsoleTextAttribute(hConsole, (WORD)(7));
-			printf("%c", res[i] ? '1' : '0');
-		}
-		SetConsoleTextAttribute(hConsole, (WORD)(15));
-	}
-	void print_base()
-	{
-		if (print_st.length()>0)
-		{
-			if (bytes == 1)
-				printf(print_st.data(), *(byte*)(buf));
-			if (bytes == 2)
-				printf(print_st.data(), *(short*)(buf));
-			if (bytes == 4)
-				printf(print_st.data(), *(int*)(buf));
-			if (bytes == 8)
-				printf(print_st.data(), *(long long*)(buf));
-		}
-	}
-	void invert_bits(int en, int count)
-	{
-		if (en >= bytes * 8)
-			en = -1;
-		for (int i = en; i > en - count && i >= 0; i--)
-		{
-			buf[i / 8] ^= (1 << (i % 8));
-		}
-
-	}
+char*col_names[] = {
+	"black",
+	"blue",
+	"green",
+	"cyan",
+	"red",
+	"magnetta",
+	"brown",
+	"lightgray",
+	"darkgray",
+	"lightblue",
+	"lightgreen",
+	"lightcyan",
+	"lightred",
+	"lightmagnetta",
+	"yellow",
+	"white"
 };
-
-
-vector < string > split_command(string str)
-{
-	vector < string > res;
-	for (int i = 0; i < str.length(); i++)
-	{
-		if (i == 0 && str[i] == ' ')
-		{
-			str.erase(i, 1);
-			i--;
-		}
-		if (i>0 && i == str.length() - 1 && str[i] == ' ')
-		{
-			str.erase(i, 1);
-			i -= 2;
-		}
-		if (i > 0 && str[i] == ' ' && str[i - 1] == ' ')
-		{
-			str.erase(i, 1);
-			i--;
-		}
-	}
-
-	str += ' ';
-
-	string resres;
-	for (int i = 0; i < str.length(); i++)
-	{
-		if (str[i] == ' ')
-		{
-			res.push_back(resres);
-			resres = "";
-		}
-		else
-		{
-			resres += str[i];
-		}
-	}
-
-
-	return res;
-}
-
-class PIX
-{
-public:
-	char sym;
-	char color;
-	char back_color;
-	char is_mig;
-	PIX()
-	{
-		sym =  '#';
-		color = 15;
-		back_color = 0;
-		is_mig = 1;
-	}
-};
-
-class WIND
-{
-public:
-	vector<PIX> arr;
-	int sizex;
-	int sizey;
-	WIND()
-	{
-		sizex = 0;
-		sizey = 0;
-	}
-	WIND(int x,int y)
-	{
-		sizex = x;
-		sizey = y;
-
-		arr.resize(x*y);
-	}
-	
-	void resize(int x, int y)
-	{
-		vector < PIX > newarr;
-		newarr.resize(x*y);
-		for (int i = 0; i < sizex&&i<x; i++)
-			for (int r = 0; r < sizey&&r < y; r++)
-			{
-				newarr[i + r*x] = arr[i + r*sizex];
-			}
-		sizex = x;
-		sizey = y;
-		arr = newarr;
-	}
-
-	void put(int x, int y,int offsetx,int offsety)
-	{
-		
-	}
-};
-
 void gotoxy(int x, int y)
 {
 	COORD scrn;
@@ -189,12 +42,6 @@ void textcol(int t, int b)
 }
 void main()
 {
-
-	TYPE_DATA number;
-	number.bytes = 0;
-	number.name = "none";
-	number.print_st = "";
-	number.buf = nullptr;
 
 	setlocale(0, "RU");
 
@@ -213,6 +60,61 @@ void main()
 	}
 	textcol(4, 3);
 	gotoxy(10, 10);
+
+
+	string text = "setInterval(function() {players[0].x += Math.random() * 10 - Math.random() * 10;players[0].y += Math.random() * 10 - Math.random() * 10;if (players[0].x< 10)players[0].x = 10;if (players[0].x>100)players[0].x = 100;if (players[0].y< 10)players[0].y = 10;if (players[0].y>100)players[0].y = 100;socket.broadcast.json.send({ 'event': 'players', 'text' : players });//alert('Прошла 1 секунда');}";
+
+	int outk = 0;
+	int x1, x2, y1, y2;
+
+	x1 = 10;
+	y1 = 12;
+	x2 = 70;
+
+	y2 = 23;
+
+	for (int r = 0; r < 16; r++)
+	{
+		textcol((r+3)%16, r);
+		cout << col_names[r] << endl;
+	}
+	system("pause");
+
+	int str_out = y1;
+
+	for (int i = 0; i < 8; i++)
+	{
+		for (int r = 0; r < 16; r++)
+		{
+			//system("cls");
+			textcol(15, 0);
+			gotoxy(0, 0);
+			cout << "                                           ";
+			gotoxy(0, 1);
+			cout << "                                           ";
+			gotoxy(0, 0);
+			cout << "text color:" << col_names[i];
+			gotoxy(0, 1);
+			cout << "text color:" << col_names[r];
+			int y = str_out;
+				for (int x = x1; x <= x2; x++)
+				{
+					gotoxy(x, y);
+					textcol(i,r);
+					cout << text[outk];
+					outk++;
+					if (outk >= text.length())
+						outk=0;
+				}
+			str_out--;
+			if (str_out < y1)
+				str_out = y2;
+			Sleep(300);
+		}
+	}
+
+
+
 	cout << "ok";
 
 	system("pause");
